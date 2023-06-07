5527637fa05b3d8c2f610fd2c214b1c81063ef9f
alif5218 <128446391+alif5218@users.noreply.github.com>
2023-06-07 11:14:00 +0600

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,8 @@
+function addStyle(){
+    var add=document.getElementById('demo');
+    add.classList.add('demo');
+}
+function removeStyle(){
+    var remove=document.getElementById('demo');
+    remove.classList.remove('demo');
+}
